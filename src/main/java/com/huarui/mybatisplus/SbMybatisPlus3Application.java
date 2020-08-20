6cb3beaf3605ba92964b7a8b8baf21a63c0ae713
lihui <youxiu326@163.com>
2020-08-20 14:02:39 +0800

    Initial commit

---
+++ src/main/java/com/huarui/mybatisplus/SbMybatisPlus3Application.java
@@ -0,0 +1,13 @@
+package com.huarui.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMybatisPlus3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbMybatisPlus3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/huarui/mybatisplus/SbMybatisPlus3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huarui.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMybatisPlus3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
